c56ae9ea3a589e8e720127b946764f68997a8d55
nverhulp <105401594+nverhulp@users.noreply.github.com>
2022-12-17 15:10:11 -0800

    added about me info

---
+++ src/components/About/About.jsx
@@ -0,0 +1,20 @@
+function About() {
+    return (
+        <section>
+            <h1 className='text-center m-2'>Hi, I'm Nicole J. Verhulp</h1>
+            <article className='mx-lg-4'>
+                <p className='m-4 aboutText'>
+                    I am a recent graduate of University of California, Davis with a major of Psychology and Political Science. 
+                </p>
+                <p className='m-4 aboutText'>
+                    I have very beginner knowledge when it comes to the coding world but I have learned a lot of the basics in the past 6 months during the Full-Stack Developer Bootcamp.
+                </p>
+                <p className='m-4 aboutText'>
+                    I am from the Bay Area and will be moving to Washington state in 2023. I am looking forward to seeing where my journey goes from here.
+                </p>
+            </article>
+        </section>
+    );
+}
+
+export default About;
