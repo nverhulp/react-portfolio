4ffb9b86cac76f3a3cc985aca515346bd3d643cc
nverhulp <105401594+nverhulp@users.noreply.github.com>
2022-12-17 17:02:00 -0800

    fixed bugs

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,41 +1,21 @@
-import React, { useState } from 'react';
-import { Document, Page } from 'react-pdf';
-import PDF from './resume.pdf';
-
+import React from 'react';
 function Resume() {
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-    setPageNumber(1);
-  }
-
-  function changePage(offset) {
-    setPageNumber(prevPageNumber => prevPageNumber + offset);
-  }
-
-  function previousPage() {changePage(-1)}
-  function nextPage() {changePage(1)}
-
   return (
-    <>
-        <p className='my-2 text-center'>You can also view and download my resume on <a href='https://drive.google.com/file/d/1tXWG6heLGfynYjTAaVqNnBYuwwtRsZWZ/view?usp=sharing' target={"_blank"} rel="noreferrer">Google Drive</a>.</p>
-        <Document file={PDF} onLoadSuccess={onDocumentLoadSuccess}>
-            <Page pageNumber={pageNumber} />
-        </Document>
-        <div className='text-center mb-3'>
-            <p>
-                Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
-            </p>
-            <button className='m-1 btn btn-secondary' type="button" disabled={pageNumber <= 1} onClick={previousPage}>
-                Previous
-            </button>
-            <button className='m-1 btn btn-secondary' type="button" disabled={pageNumber >= numPages} onClick={nextPage}>
-                Next
-            </button>
+    <section className="my-1">
+      <div className="d-flex justify-content-center">
+        <div className="card text-center w-75">
+          <div className="card-header m-1 textFontColor font-weight-bold">My Resume:</div>
+          <div className="card-body justify-content-left">
+            <a
+              className="list-group-item list-group-item-action active text-white "
+              href="https://drive.google.com/file/d/1tXWG6heLGfynYjTAaVqNnBYuwwtRsZWZ/view?usp=sharing"
+              target="blank">
+              Download my Resume
+            </a>
+          </div>
+        </div>
       </div>
-    </>
+    </section>
   );
 }
 
